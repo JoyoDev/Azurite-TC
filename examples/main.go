f1b0f9a4fb7b775552a098da5b3b9a28317d4ea2
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-05-26 16:02:28 +0200

    Add examples

---
+++ examples/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	atc "github.com/joyodev/azurite-tc"
+)
+
+func main() {
+
+	azuriteTC := atc.NewAzuriteTC("devstoreaccount1", "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==")
+	
+	// create container
+	azuriteTC.RunAzuriteContainer()
+
+	// create test table and work with it
+	azuriteTC.CreateTable("test")
+	azuriteTC.UpdateTableValue("test", "user", "activeuser", "1")
+
+	// read value
+	value, err := azuriteTC.GetTableValue("test", "user", "activeuser")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	} else {
+		fmt.Println(value)
+	}
+
+	// update again
+	azuriteTC.UpdateTableValue("test", "user", "activeuser", "2")
+
+	// read again
+	value, err = azuriteTC.GetTableValue("test", "user", "activeuser")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	} else {
+		fmt.Println(value)
+	}
+
+	// remove azurite container
+	azuriteTC.RemoveAzuriteContainer()
+}
